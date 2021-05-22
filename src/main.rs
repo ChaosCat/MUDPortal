8419c2057da0b92d1edf26f982239e3c55a6f725
Elian Kamal <shady3300@outlook.com>
2021-05-22 10:10:13 +0300

    Reflect naming changes to fix IDEA build

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use] extern crate log;
+#[macro_use] extern crate simple_logger;
+
 pub mod client;
 pub mod network;
 
@@ -7,15 +10,31 @@ use std::convert::TryFrom;
 use std::fmt::{Error, Display, Formatter};
 use crate::client::morgengrauen::client_loop;
 use crate::client::consts::MORGENGRUEN_CONNECTION_INFO;
+use simple_logger::SimpleLogger;
+use log::SetLoggerError;
 
 // TODO: A specialized error encompassing possibilities
 
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
 
-    let mut connection = Telnet::connect(MORGENGRUEN_CONNECTION_INFO,
-                                         1028).expect("Server connection failed");
+    match SimpleLogger::new().init() {
+        Ok(_) => info!("Logger initialized"),
+        Err(err) => {
+            eprintln!("Logger initialization failed with error: {}", err);
+            return Ok(())
+        }
+    }
 
+    let mut connection = match Telnet::connect(MORGENGRUEN_CONNECTION_INFO,
+                                         1028) {
+        Ok(conn) => conn,
+        Err(err) => {
+            eprintln!("Connection establishment failed!");
+            return Ok(())
+        }
+    };
+    
     client_loop(connection);
 
     Ok(())
